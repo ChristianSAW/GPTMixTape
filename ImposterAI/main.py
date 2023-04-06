2eefb966062d0d3b7847b13fd12573ff000d5577
Christian Welling <csw73@cornell.edu>
2023-04-05 19:02:53 -0700

    Test out using dash

--- ImposterAI/main.py
+++ ImposterAI/main.py
@@ -1,15 +1,61 @@
 from flask import Flask, render_template, request, jsonify
+from dash import Dash, html, dcc, callback
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import pandas as pd
 import openai
 import os
+import sys
 
-app = Flask(__name__)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-with open("API_KEY") as f:
-    file_contents = f.read()
+#app = Flask(__name__)
+app = Dash(__name__)
 
-print(file_contents)
-os.environ["OPENAI_API_KEY"] = file_contents
 
+
+try:
+    with open("API_KEY") as f:
+        file_contents = f.read()
+
+    print(file_contents)
+    os.environ["OPENAI_API_KEY"] = file_contents
+except:
+    from SecureKeys import gpt_api_key
+    openai.api_key = gpt_api_key
+
+
+
+#region Basic Dash App
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    html.Div(
+        [
+            dbc.Input(id="input", placeholder="Type something...", type="text"),
+            html.Br(),
+            html.P(id="output"),
+        ]
+    ),
+    html.Div(
+        [
+            dbc.Button("Send", id="send-button", color="primary", className="me-1")
+        ]
+    )
+    ])
+
+@app.callback(
+        Output("output", "children"),
+        Input('send-button', 'n_clicks'),
+        State("input", "value")
+    )
+def output_text(n_clicks, value):
+    sys_message ="Respond as if your best friend."
+    out = send_message_to_chat_gpt(sys_message, value)
+    return out
+    
+#endregion Basic Dash App
+
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -22,6 +68,7 @@ def message():
     chat_gpt_response = send_message_to_chat_gpt(sys_message, user_message)
     return jsonify(chat_gpt_response)
 
+'''
 def send_message_to_chat_gpt(sys_message, user_message):
     try:
         response = openai.ChatCompletion.create(
@@ -40,4 +87,4 @@ def send_message_to_chat_gpt(sys_message, user_message):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
