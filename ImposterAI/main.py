1c6856920d406d67d922a421e52d32326fc78b73
trd44 <trd44@cornell.edu>
2023-04-05 21:57:17 -0400

    Created two separate text boxes for submitting both system and user messages to chat GPT also the messages are all saved in a list now  and the history is sent to chatGPT to give it context

--- ImposterAI/main.py
+++ ImposterAI/main.py
@@ -9,31 +9,40 @@ with open("API_KEY") as f:
 
 print(file_contents)
 os.environ["OPENAI_API_KEY"] = file_contents
+saved_messages = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/message', methods=['POST'])
-def message():
-    user_message = request.form['message']
-    sys_message = request.form['custom_text']
-    # message = custom_text + user_message
-    chat_gpt_response = send_message_to_chat_gpt(sys_message, user_message)
+@app.route('/system-message', methods=['POST'])
+def system_message():
+    # print("System Message Called")
+    sys_message = request.form['system_message']
+    save_message({"role": "system", "content": sys_message})
+    chat_gpt_response = send_message_to_chat_gpt()
     return jsonify(chat_gpt_response)
 
-def send_message_to_chat_gpt(sys_message, user_message):
+@app.route('/user-message', methods=['POST'])
+def user_message():
+    user_message = request.form['user_message']
+    save_message({"role": "user", "content": user_message})
+    chat_gpt_response = send_message_to_chat_gpt()
+    return jsonify(chat_gpt_response)
+
+def save_message(msg):
+    saved_messages.append(msg)
+
+def send_message_to_chat_gpt():
     try:
         response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-                {"role": "system", "content": sys_message},
-                {"role": "user", "content": user_message}
-            ]
+        messages=saved_messages
         )
-        # print(response)
-        message = response.choices[0].message.content
-        return {'status': 'success', 'message': message}
+        chat_gpt_response = response.choices[0].message.content
+        save_message({"role": "assistant", "content": chat_gpt_response})
+        # print(saved_messages)
+        return {'status': 'success', 'message': chat_gpt_response}
 
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
--- ImposterAI/static/js/scripts.js
+++ ImposterAI/static/js/scripts.js
@@ -1,43 +1,66 @@
 $(document).ready(function () {
     const chatWindow = $("#chat-window");
-    const chatForm = $("#chat-form");
+    const systemForm = $("#system-form");
+    const userForm = $("#user-form");
 
-    function appendMessage(message, className) {
-        const messageElement = $(`<div class="chat-message ${className}">${message}</div>`);
+    function appendMessage(message, className, initials) {
+        const messageElement = $(`
+            <div class="chat-message ${className}">
+                <div class="initials">${initials}</div>
+                <div class="content">${message}</div>
+            </div>
+        `);
         chatWindow.append(messageElement);
         chatWindow.scrollTop(chatWindow[0].scrollHeight);
     }
 
-    chatForm.on('submit', function (event) {
-        // event.preventDefault();
-        if (event.type === 'submit' || (event.type === 'keydown' && event.keyCode === 13)) {
-            event.preventDefault();
-        }
-        
-        const userMessage = $('#chat-input').val();
-
-        if (userMessage.trim() !== '') {
-            appendMessage(userMessage, 'user-message');
-            $('#chat-input').val('');
-
-            $.ajax({
-                type: 'POST',
-                url: '/message',
-                data: {
-                    message: userMessage,
-                    custom_text: $('#custom-textbox').val()
-                },                
-                success: function (response) {
-                    if (response.status === 'success') {
-                        appendMessage(response.message, 'assistant-message');
-                    } else {
-                        appendMessage('Error: Unable to get response from ChatGPT.', 'assistant-message');
-                    }
-                },
-                error: function () {
-                    appendMessage('Error: Unable to connect to server.', 'assistant-message');
+    systemForm.on('submit', function(event) {
+        console.log('System form submitted!');
+        event.preventDefault();
+        const sysMessage = $('#system-input').val();
+        appendMessage(sysMessage, 'system-message', 'SYS')
+        $.ajax({
+            type: 'POST',
+            url: '/system-message',
+            data: {
+                system_message: sysMessage
+            },
+            success: function (response) {
+                if (response.status === 'success') {
+                    appendMessage(response.message, 'assistant-message', 'Chat');
+                } else {
+                    appendMessage('Error: Unable to get response from ChatGPT.', 'assistant-message', 'Err');
                 }
-            });
-        }
+            },
+            error: function () {
+                appendMessage('Error: Unable to connect to server.', 'assistant-message', 'Err');
+            }
+        });
+        // $('#system-input').val('');
+    });
+
+    userForm.on('submit', function(event) {
+        console.log('User form submitted!');
+        event.preventDefault();
+        const userMessage = $('#user-input').val();
+        appendMessage(userMessage, 'user-input', 'User')
+        $.ajax({
+            type: 'POST',
+            url: '/user-message',
+            data: {
+                user_message: userMessage
+            },
+            success: function (response) {
+                if (response.status === 'success') {
+                    appendMessage(response.message, 'user-message', 'Chat');
+                } else {
+                    appendMessage('Error: Unable to get response from ChatGPT.', 'user-message', 'Err');
+                }
+            },
+            error: function () {
+                appendMessage('Error: Unable to connect to server.', 'user-message', 'Err');
+            }
+        });
+        $('#user-input').val('');
     });
 });
